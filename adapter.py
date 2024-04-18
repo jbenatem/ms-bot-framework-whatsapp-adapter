fd4807a1acb6e81e78785ff4f382a0f491687f6b
Juan Benate <jbenatem@gmail.com>
2024-04-18 16:37:05 -0500

    first commit, controller to send messages to whatsapp added

---
+++ adapter.py
@@ -0,0 +1,37 @@
+from flask import Flask, request
+import requests
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/whatsapp/api/send", methods=['POST'])
+def send_message_to_whatsapp():
+    data = request.get_json()
+    if (data != None):
+        senderId = data.get("senderId")
+        token = data.get("token")
+        message = data.get("message")
+    
+    url = "https://graph.facebook.com/v18.0/" + senderId + "/messages"
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + token
+    }
+    payload = json.dumps(message)
+    
+    response = requests.request("POST", url, headers=headers, data=payload)
+    
+    if response.status_code == 200:
+        return response.text
+    elif response.status_code <= 499 and response.status_code >= 400: 
+        return "<h2>Error por parte del cliente</h2>"
+    elif response.status_code <= 599 and response.status_code >= 500: 
+        return "<h2>Error por parte del servidor</h2>"
+    else: 
+        return "<h2>Error desconocido</h2>"
+    
+    return response.text
